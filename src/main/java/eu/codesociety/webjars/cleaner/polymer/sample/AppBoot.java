7186c3919f281bb68ffc78bd7bd5b2060fd77853
bartolom <bart@nice-person.net>
2016-08-09 23:31:50 +0200

    Typical Spring Boot 1.4.0 setup

---
+++ src/main/java/eu/codesociety/webjars/cleaner/polymer/sample/AppBoot.java
@@ -0,0 +1,12 @@
+package eu.codesociety.webjars.cleaner.polymer.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+@EnableAutoConfiguration
+public class AppBoot {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppBoot.class, args);
+	}
+}
