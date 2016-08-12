a7f8093293d032df614b0e5a434525b440aa88e3
bartolom <bart@nice-person.net>
2016-08-12 21:08:49 +0200

    Use of the Cleaner and Packs in the sample Spring Boot application

--- src/main/java/eu/codesociety/webjars/cleaner/polymer/sample/AppConfigMvc.java
+++ src/main/java/eu/codesociety/webjars/cleaner/polymer/sample/AppConfigMvc.java
@@ -4,6 +4,8 @@ import java.util.SortedMap;
 import java.util.function.Function;
 import java.util.regex.Pattern;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -12,16 +14,34 @@ import org.springframework.web.servlet.resource.PathResourceResolver;
 import org.springframework.web.servlet.resource.WebJarsResourceResolver;
 import org.webjars.WebJarAssetLocator;
 
+import eu.codesociety.webjars.cleaner.polymer.PackForPolymer;
+import eu.codesociety.webjars.cleaner.polymer.PackForSaulis;
+
 @Configuration
 public class AppConfigMvc extends WebMvcConfigurerAdapter {
 
+	private static final Logger logger = LoggerFactory.getLogger(AppConfigMvc.class);
+	
 	@Bean
 	public WebJarAssetLocator webJarAssetLocator() {
 		SortedMap<String, String> index = WebJarAssetLocator.getFullPathIndex(
 				Pattern.compile(".*"), 
-				WebJarAssetLocator.class.getClassLoader());
+				AppConfigMvc.class.getClassLoader());
+		
+		logger.info("Full index size {}", index.size());
 		Function<String, String> f = a -> (a.equals("polymer")) ? "github-com-Polymer-polymer" : a;
-		WebJarAssetLocator locator = new WebJarAssetLocator(index, a -> f.apply(a));
+		
+		SortedMap<String, String> cleanedIndex = 
+				(new PackForPolymer())
+						.andThen(new PackForSaulis())
+						.apply(index);
+		
+		logger.info("Reduced from {} to {}", index.size(), cleanedIndex.size());
+		cleanedIndex.forEach((k, v) -> logger.info("  - {}", k));
+		
+		logger.info("Cleaned index size {}", cleanedIndex.size());
+		
+		WebJarAssetLocator locator = new WebJarAssetLocator(cleanedIndex, a -> f.apply(a));
 		return locator;
 	}
 	
@@ -30,7 +50,7 @@ public class AppConfigMvc extends WebMvcConfigurerAdapter {
 
 		WebJarsResourceResolver resolver = new WebJarsResourceResolver(webJarAssetLocator());
 		
-		registry.addResourceHandler("/bower/**")
+		registry.addResourceHandler("/bower_components/**")
 				.addResourceLocations("classpath:META-INF/resources/webjars/")
 				.resourceChain(true)
 				.addResolver(resolver)
